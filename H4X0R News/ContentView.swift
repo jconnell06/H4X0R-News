58673fd276e18fd7029be5ad10ff1b3b3ae31dec
JoEllen Connell <connell3@purdue.edu>
2020-05-09 16:48:22 -0400

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by JoEllen Connell on 5/9/20.
+//  Copyright © 2020 JoEllen Connell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
